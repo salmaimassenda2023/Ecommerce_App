6c11d307782e1eace9d7594214af9d590cf2a01d
SALMA IMASSENDA <salmaimassenda2023@gmail.com>
2025-02-14 07:41:01 +0100

    add components folder

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HeroBanner=()=>{
+    return(
+        <div>
+         HeroBanner
+        </div>
+    )
+}
+export default HeroBanner
---
+++ components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Layout=()=>{
+    return(
+        <div>
+         layout
+        </div>
+    )
+}
+export default Layout
