821af679cc4e8ba122982278b35fe52d63e3997c
imassenda <salmaimassenda2023@gmail.com>
2025-02-20 10:23:37 +0100

    Footerr Component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { AiFillInstagram } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
 
 const Footer=()=>{
     return(
-        <div>
-         Footer
+        <div className="footer-container">
+            <p>@2025 IMA Headphones All rights reserved </p>
+            <p className="icons">
+                <AiFillInstagram />
+                <AiOutlineTwitter />
+            </p>
         </div>
+
     )
 }
 export default Footer
